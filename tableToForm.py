bd77ad6127cd7cd60509b714e8aaa785495ca0e3
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-05-01 00:39:53 +0400

    Реализация GUI без возможности выбора папки сохранения

--- tableToForm.py
+++ tableToForm.py
@@ -1,9 +1,33 @@
 from fuctions import *
+import PySimpleGUI as sg
 
-path_xl = Path('tab.xlsx')  # Path(input('Введите путь до таблицы с данными: '))
-path_wd = Path('template.docx')  # Path(input('Введите путь до шаблона: '))
-format = input('Введите формат конечных файлов (PDF / docx): ')
-format = format if format else 'pdf'
+sg.theme('LightGrey1')
+layout = [[sg.Text('Таблица', size=(14, 1)), sg.InputText(), sg.FileBrowse('Обзор', file_types=(('Excel', '*.xlsx'),), size=(10, 1))],
+          [sg.Text('Шаблон', size=(14, 1)), sg.InputText(), sg.FileBrowse(
+              'Обзор', file_types=(('Word', '*.docx'),), size=(10, 1))],
+          [sg.Text('Папка сохранения', size=(14, 1)), sg.InputText(),
+           sg.FolderBrowse('Обзор', size=(10, 1))],
+          [sg.Text('Формат сохранения', size=(14, 1)), sg.Radio('pdf', 'FORMAT', default=True), sg.Radio('docx', 'FORMAT'), sg.Submit('Старт!', size=(10, 1))]]
 
-contexts = readInfoFromBook(path_xl)
-renderTpl(path_wd, contexts, format)
+window = sg.Window('Table To Form', layout)
+
+while True:
+    event, values = window.read()
+
+    if event in (None, 'Cancel'):
+        break
+
+    print('You entered ', values[0], values[1],
+          values[2], values[3], values[4])
+    path_xl = Path(values[0])
+    path_wd = Path(values[1])
+
+    if values[3]:
+        format = 'pdf'
+    else:
+        format = 'docx'
+
+    contexts = readInfoFromBook(path_xl)
+    renderTpl(path_wd, contexts, format)
+
+window.close()
